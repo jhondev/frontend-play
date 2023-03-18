378236ea5673c0945963613b17701ce8b2f0b9d9
jhondev <jma9974@gmail.com>
2023-03-17 21:25:20 -0500

    sveltekit hello app

---
+++ sveltekit/hello/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Presenter from "./Presenter.svelte";
+</script>
+<h1>Welcome to SvelteKit</h1>
+<Presenter></Presenter>
---
+++ sveltekit/hello/src/routes/Presenter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	type User = {
+		name: string;
+	};
+	let user: User = { name: '' };
+</script>
+
+Enter presenter name
+<input type="text" name="ptr" id="ptr" bind:value={user.name} />
+<div>Testing Presenter {user.name}</div>
