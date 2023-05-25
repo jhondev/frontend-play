bdd6f76b0b4721e5ac4664664e79dd997f954e25
jhondev <jma9974@gmail.com>
2023-05-24 21:10:04 -0500

    register wip

--- sveltekit/with-pocketbase/apps/web/src/routes/+layout.svelte
+++ sveltekit/with-pocketbase/apps/web/src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 			{#if !data.user}
 				<div class="dropdown dropdown-end">
 					<a href="/login" class="btn btn-primary">Login</a>
-					<a href="/register" class="btn btn-primary">Register</a>
+					<a href="/register" class="btn btn-secondary">Register</a>
 				</div>
 			{:else}
 				<div class="dropdown dropdown-end mr-4">
@@ -24,9 +24,28 @@
 							<img src="https://placeimg.com/80/80/people" alt="user avatar" />
 						</div>
 					</label>
+					<ul
+						class="menu menu-compact drowpdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+					>
+						<li>
+							<a href="/my/projects" class="justify-between">My Projects</a>
+						</li>
+						<li>
+							<a href="/my/settings">Settings</a>
+						</li>
+						<li>
+							<form action="/logout" method="POST">
+								<button type="submit" class="w-full text-start">Logout</button>
+							</form>
+						</li>
+					</ul>
 				</div>
 			{/if}
 		</div>
 	</nav>
-	<slot />
+	<div class="py-10">
+		<div class="max-auto max-w-7xl sm:px-6 lg:px-8">
+			<slot />
+		</div>
+	</div>
 </div>
